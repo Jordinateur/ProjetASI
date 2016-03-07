2a18c651efd29ba3cd58eb128fdd2fcb9660d629
Jordinateur <jordan.gaudron@gmail.com>
2016-03-07 11:49:21 +0100

    jndi

--- HockeyWEB/src/hs/utility/LookUpJNDI.java
+++ HockeyWEB/src/hs/utility/LookUpJNDI.java
@@ -29,5 +29,19 @@ public class LookUpJNDI {
         return remote;
 
 	}
+	public static GardienManagerRemote lookupManager() throws NamingException{
+		GardienManagerRemote remote=null;
+        try {
+            Properties jndiProps = new Properties();
+            jndiProps.setProperty(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+            //jndiProps.setProperty(Context.PROVIDER_URL, "jnp://localhost:4447");
+            InitialContext ctx = new InitialContext(jndiProps); 
+            remote = (GardienManagerRemote) ctx.lookup("java:global/HockeyEAR/HockeyEJB/GardienManager!hs.ejb.GardienManagerRemote");
+        	} catch (Exception e) {
+        		e.printStackTrace();
+        	}    	
+        return remote;
+
+	}
 
 }
